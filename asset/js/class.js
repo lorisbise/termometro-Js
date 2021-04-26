e232f4b2fe24b409ac512ef20229543ab4268510
Loris Bisesti <76796703+lorisbise@users.noreply.github.com>
2021-04-26 15:02:49 +0200

    Add files via upload

---
+++ asset/js/class.js
@@ -0,0 +1,13 @@
+class Termometro {
+
+  constructor(misura, storico, celsius, fahrenheit, tipo, report) {
+    this.misura = misura;
+    this.storico = storico;
+    this.celsius  = celsius;
+    this.fahrenheit = fahrenheit;
+    this.tipo = tipo;
+    this.report = report;
+
+  }
+}
+let termometro = new Termometro(0,[])
